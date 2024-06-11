bcfbe9c6c93705ad13d726ccb37e71d72a2a6554
satish1922 <115685275+satish1922@users.noreply.github.com>
2024-06-11 10:10:29 +0530

    Add files via upload

---
+++ LAB-1/PrimitiveDataTypes.java
@@ -0,0 +1,29 @@
+package projectanudip;
+
+public class PrimitiveDataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			byte byteValue = 127;
+	        short shortValue = 32767;
+	        int intValue = 2147483647;
+	        long longValue = 9223372036854775807L;
+	        
+	        float floatValue = 3.4e+38f;
+	        double doubleValue = 1.7e+308;
+	        
+	        char charValue = 'A';
+	        
+	        boolean booleanValue = true;
+	        
+	        System.out.println("byteValue = " + byteValue);
+	        System.out.println("shortValue = " + shortValue);
+	        System.out.println("intValue = " + intValue);
+	        System.out.println("longValue = " + longValue);
+	        System.out.println("floatValue = " + floatValue);
+	        System.out.println("doubleValue = " + doubleValue);
+	        System.out.println("charValue = " + charValue);
+	        System.out.println("booleanValue = " + booleanValue);
+	}
+
+}
---
+++ LAB-1/SwapAndAdd.java
@@ -0,0 +1,18 @@
+package projectanudip;
+
+public class SwapAndAdd {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("Swapped numbers:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("Addition of two numbers: " + (a + b));
+    }
+}
