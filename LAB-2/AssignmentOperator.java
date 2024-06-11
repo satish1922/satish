e7d58f6da1305be9a7147eaa7ab88b2c74305151
satish1922 <115685275+satish1922@users.noreply.github.com>
2024-06-11 10:10:44 +0530

    Add files via upload

---
+++ LAB-2/AssignmentOperator.java
@@ -0,0 +1,27 @@
+package projectanudip;
+import java.io.*;
+public class AssignmentOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int a = 10;
+	        int b = 5;
+
+	        System.out.println("Value of c after assignment: " + (a));
+	        System.out.println("Value of c after addition: " + (a += b));
+	        System.out.println("Value of c after subtraction: " + (a -= b));
+	        System.out.println("Value of c after multiplication: " + (a *= b));
+	        System.out.println("Value of c after division: " + (a /= b));
+	        System.out.println("Value of c after modulus: " + (a %= b));
+
+	        int x = 5;
+	        System.out.println("Value of x after bitwise AND: " + (x &= 3));
+	        System.out.println("Value of x after bitwise OR: " + (x |= 3));
+	        System.out.println("Value of x after bitwise XOR: " + (x ^= 3));
+	        System.out.println("Value of x after left shift: " + (x <<= 1));
+	        System.out.println("Value of x after right shift: " + (x >>= 1));
+	        System.out.println("Value of x after zero fill right shift: " + (x >>>= 1));
+	    
+	}
+
+}
---
+++ LAB-2/LoginUserCheck.java
@@ -0,0 +1,21 @@
+package projectanudip;
+import java.util.Scanner;
+public class LoginUserCheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your username: ");
+        String username = scanner.nextLine();
+
+        System.out.println("Enter your password: ");
+        String password = scanner.nextLine();
+
+        if (username.equals("admin") && password.equals("password")) {
+            System.out.println("You are logged in!");
+        } else {
+            System.out.println("Invalid username or password.");
+        }
+    }
+}
